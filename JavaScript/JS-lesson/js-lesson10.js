52237d96d68fe98631041dd0b4a0d76ca27d624c
nozomi <nozobsk0720@i.softbank.jp>
2024-10-21 21:15:58 +0900

    test

---
+++ JavaScript/JS-lesson/js-lesson10.js
@@ -0,0 +1,10 @@
+//youageの値を好きな数字に変更してどうなるか試してみましょう！
+
+let youage =24;
+let centerforce = youage < 26;
+
+if (centerforce) {
+  console.log('株式会社centerforceの平均年齢以下です!');
+} else {
+  console.log('株式会社centerforceの平均年齢を超えています!');
+}
---
+++ JavaScript/JS-lesson/js-lesson11.js
@@ -0,0 +1,12 @@
+//jankenの値を好きな’チョキ’と’パー’に変更してどうなるか試してみましょう！
+
+let janken ='グー';
+
+if (janken=='チョキ') {
+  console.log('私の手はチョキです！');
+} else if (janken=='パー'){
+    console.log('私の手はパーです！');
+}else {
+  console.log('私の手はグーです！');
+}
+
